2a7f2e4c0339862faa1d4c9648e670c9a3f07575
Sudhars Chinnu <sudharschinnusamy06@gmail.com>
2025-12-13 10:19:28 +0530

    Point API host to Render backend

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,5 +1,5 @@
 // const host = "https://expense-tracker-app-knl1.onrender.com";
-export const host = "http://localhost:5000";
+export const host = "https://expense-manager-gqtt.onrender.com";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
